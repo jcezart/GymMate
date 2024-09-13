7f8e7339a36b5ddfbd93eebb8a5b9be0d41f7281
jcezart <jcezar.t@gmail.com>
2024-09-13 08:31:20 -0300

    ajuste de anotação API26

--- app/src/main/java/com/example/gymmate/MainActivity.kt
+++ app/src/main/java/com/example/gymmate/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.gymmate
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import com.example.gymmate.data.ExerciseDAO
@@ -16,6 +18,7 @@ class MainActivity : ComponentActivity() {
 
 
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
