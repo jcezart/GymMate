c45825de2ae7edb51625155126c7226512e5a7db
juliocezar <jcezar.t@gmail.com>
2025-02-21 00:09:25 -0300

    new features

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,11 +10,11 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.gymmate"
+        applicationId = "com.juliocezar.gymmate"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
